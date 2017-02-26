3b1465c32f1a51d410531270c4335556dc7d1043
Aschur <suyundukovyf1@yandex.ru>
2017-02-26 13:56:20 +0300

    connect to hibernate 1

--- Aschur_Store/src/management/Manager.java
+++ Aschur_Store/src/management/Manager.java
@@ -7,8 +7,10 @@ import gui.TellerWorkingPlace;
 import product.*;
 
 import javax.swing.*;
+
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Map;
 import java.util.Observable;
 import java.util.Random;
 import java.util.concurrent.ArrayBlockingQueue;
@@ -18,198 +20,253 @@ import java.util.concurrent.ArrayBlockingQueue;
  */
 public class Manager {
 
-    public static void main(String[] args) throws Exception{
+	private static String modeDataStorage = "";
+
+	public static void main(String[] args) throws Exception {
+
+		Map<String, String> primarySettings = SettingsLoader
+				.getPrimarySettings();
+
+		String modeDataStorage = primarySettings.get("modeDataStorage");
+		if (modeDataStorage.equals("fileApp")
+				|| modeDataStorage.equals("dataBase")) {
+			setModeDataStorage(modeDataStorage);
+		} else {
+			System.out.println("modeDataStorage parametr is undefined");
+			return;
+		}
+
+		String usageMode = primarySettings.get("usageMode");
+		if (!(usageMode.equals("handMode") || usageMode.equals("automaticMode"))) {
+			System.out.println("usageMode parametr is undefined");
+			return;
+		}
+
+		Map<String, String> settings = SettingsLoader.getSettings();
+
+		int countConsumers = 0;
+		try {
+			countConsumers = Integer.parseInt(settings.get("countConsumers"));
+		} catch (Exception e) {
+			System.out.println("countConsumers parametr is undefined");
+			return;
+		}
+
+		int maxCountEnterStore = 0;
+		try {
+			maxCountEnterStore = Integer.parseInt(settings
+					.get("maxCountEnterStore"));
+		} catch (Exception e) {
+			System.out.println("maxCountEnterStore parametr is undefined");
+			return;
+		}
 
-        String[] settings;
-        if (args.length > 0)
-            settings = args;
-        else
-            settings = SettingsLoader.getSettings();
+		int timeWorkStore = 0;
+		try {
+			timeWorkStore = Integer.parseInt(settings.get("timeWorkStore"));
+		} catch (Exception e) {
+			System.out.println("timeWorkStore parametr is undefined");
+			return;
+		}
 
-        if (settings.length != 6){
-            System.out.println("the number of input parameters must equal 6");
-            return;
-        }
+		ShelfOfConsumer shelfOfConsumer = new ShelfOfConsumer();
+		ProductsDataBaseOfSeller productsDataBaseOfSeller = new ProductsDataBaseOfSeller();
+		DelayProducts delayProducts = new DelayProducts();
 
-        String fileNameProcurementPlan = settings[0];
-        String fileNameProducts = settings[1];
-        int countConsumers = Integer.parseInt(settings[2]);
-        int maxCountEnterStore = Integer.parseInt(settings[3]);
-        int timeWorkStore = Integer.parseInt(settings[4]);
-        String usageMode = settings[5];
+		if (modeDataStorage.equals("fileApp")) {
 
-        ArrayBlockingQueue<Consumer> waitingStoreConsumers = new ArrayBlockingQueue<Consumer>(countConsumers);
-        final ConsumersQueueTeller standingLineConsumers = new ConsumersQueueTeller(countConsumers);
+			String fileNameProcurementPlan = settings
+					.get("fileNameProcurementPlan");
+			if (fileNameProcurementPlan.equals("")) {
+				System.out
+						.println("fileNameProcurementPlan parametr is undefined");
+				return;
+			}
 
-        ProductsGeneratorFromTextFile.readAllString(fileNameProducts);
-        RandomGeneratorProcurementPlan.readAllStringFromFile(fileNameProcurementPlan);
+			String fileNameProducts = settings.get("fileNameProducts");
+			if (fileNameProducts.equals("")) {
+				System.out.println("fileNameProducts parametr is undefined");
+				return;
+			}
 
-        ShelfOfConsumer shelfOfConsumer = new ShelfOfConsumer();
-        ProductsDataBaseOfSeller productsDataBaseOfSeller = new ProductsDataBaseOfSeller();
-        DelayProducts delayProducts = new DelayProducts();
+			ProductsGeneratorFromTextFile.readAllString(fileNameProducts);
+			RandomGeneratorProcurementPlan
+					.readAllStringFromFile(fileNameProcurementPlan);
 
-        while (ProductsGeneratorFromTextFile.hasNext()){
-            TupleOfProducts tupleOfProducts = ProductsGeneratorFromTextFile.next();
-            shelfOfConsumer.put(tupleOfProducts.productOfConsumer, tupleOfProducts.productOfConsumerCount);
-            productsDataBaseOfSeller.put(tupleOfProducts.productOfSeller, tupleOfProducts.productOfSellerCount);
-        }
+			while (ProductsGeneratorFromTextFile.hasNext()) {
+				TupleOfProducts tupleOfProducts = ProductsGeneratorFromTextFile
+						.next();
+				shelfOfConsumer.put(tupleOfProducts.productOfConsumer,
+						tupleOfProducts.productOfConsumerCount);
+				productsDataBaseOfSeller.put(tupleOfProducts.productOfSeller,
+						tupleOfProducts.productOfSellerCount);
+			}
 
+			if (shelfOfConsumer.isEmpty() || productsDataBaseOfSeller.isEmpty()) {
 
-        if (shelfOfConsumer.isEmpty() || productsDataBaseOfSeller.isEmpty()){
+				System.out.println("failed to load products from a file");
+				return;
 
-            System.out.println("failed to load products from a file");
-            return;
+			}
 
-        }
+		}
 
-        ArrayList<Consumer> consumerArrayList = new ArrayList<Consumer>();
+		ArrayBlockingQueue<Consumer> waitingStoreConsumers = new ArrayBlockingQueue<Consumer>(
+				countConsumers);
+		final ConsumersQueueTeller standingLineConsumers = new ConsumersQueueTeller(
+				countConsumers);
 
-        for (int i = 0; i < countConsumers ; i++) {
-            Consumer consumer = new Consumer(standingLineConsumers, shelfOfConsumer);
-            Thread consumerThread = new Thread(consumer, consumer.getName());
-            waitingStoreConsumers.add(consumer);
+		ArrayList<Consumer> consumerArrayList = new ArrayList<Consumer>();
 
-            consumerArrayList.add(consumer);
+		for (int i = 0; i < countConsumers; i++) {
+			Consumer consumer = new Consumer(standingLineConsumers,
+					shelfOfConsumer);
+			Thread consumerThread = new Thread(consumer, consumer.getName());
+			waitingStoreConsumers.add(consumer);
 
-            consumerThread.start();
-        }
+			consumerArrayList.add(consumer);
 
+			consumerThread.start();
+		}
 
-        final BarcodeReader barcodeReader = new BarcodeReader();
+		final BarcodeReader barcodeReader = new BarcodeReader();
 
-        ArrayList<Observable> observables = new ArrayList<Observable>();
-        observables.add(barcodeReader);
+		ArrayList<Observable> observables = new ArrayList<Observable>();
+		observables.add(barcodeReader);
 
-        final CashRegister cashRegister = new CashRegister(observables, productsDataBaseOfSeller, delayProducts);
+		final CashRegister cashRegister = new CashRegister(observables,
+				productsDataBaseOfSeller, delayProducts);
 
+		Teller teller = null;
+		Thread tellerThread = null;
 
+		if (usageMode.equals("handMode")) {
 
-        Teller teller = null;
-        Thread tellerThread = null;
+			SwingUtilities.invokeLater(new Runnable() {
+				public void run() {
 
-        if (usageMode.equals("handMode")){
+					new TellerWorkingPlace("TellerWorkingPlace",
+							standingLineConsumers, cashRegister, barcodeReader);
 
+				}
 
-            SwingUtilities.invokeLater(new Runnable() {
-                public void run() {
+			});
 
-                    new TellerWorkingPlace("TellerWorkingPlace",
-                            standingLineConsumers, cashRegister, barcodeReader);
+		} else {
 
-                }
+			teller = new Teller(standingLineConsumers, cashRegister,
+					barcodeReader);
+			tellerThread = new Thread(teller, teller.getName());
 
-            });
+		}
 
+		Date beginWork = new Date();
 
+		if (usageMode.equals("automaticMode")) {
 
-        }else{
+			teller.setState(StatesTeller.WORKS);
+			tellerThread.start();
 
-            teller = new Teller(standingLineConsumers, cashRegister, barcodeReader);
-            tellerThread = new Thread(teller, teller.getName());
+		}
 
-        }
+		int countcon = countConsumers;
+		while (countcon != 0) {
 
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				stopWork(teller, standingLineConsumers, waitingStoreConsumers);
+				return;
+			}
 
+			int maxCountEnterStore1 = StrictMath.min(countcon,
+					maxCountEnterStore);
 
-        Date beginWork = new Date();
+			Random rand = new Random();
+			int countEnterStore = rand.nextInt(maxCountEnterStore1 + 1);
 
-        if (usageMode.equals("automaticMode")){
+			int countEnterStoreGood = 0;
 
-            teller.setState(StatesTeller.WORKS);
-            tellerThread.start();
+			for (int i = 0; i < countEnterStore; i++) {
+				Consumer consumer = waitingStoreConsumers.poll();
+				if (consumer != null) {
 
-        }
+					consumer.setState(StatesConsumer.TAKE);
+					countEnterStoreGood++;
 
+				}
 
+			}
 
-        int countcon = countConsumers;
-        while (countcon != 0){
+			countcon -= countEnterStoreGood;
 
-            try {
-                Thread.sleep(100);
-            }catch (InterruptedException e){
-                stopWork(teller, standingLineConsumers, waitingStoreConsumers);
-                return;
-            }
+		}
 
-            int maxCountEnterStore1 = StrictMath.min(countcon, maxCountEnterStore);
+		while (true) {
 
-            Random rand = new Random();
-            int countEnterStore = rand.nextInt(maxCountEnterStore1 + 1);
+			Date currentDate = new Date();
+			long elapsedTime = currentDate.getTime() - beginWork.getTime();
+			if (elapsedTime < (long) timeWorkStore) {
 
-            int countEnterStoreGood = 0;
+				try {
+					Thread.sleep(1000);
+					continue;
+				} catch (InterruptedException e) {
+					stopWork(teller, standingLineConsumers,
+							waitingStoreConsumers);
+					return;
+				}
 
-            for (int i = 0; i < countEnterStore; i++) {
-                Consumer consumer = waitingStoreConsumers.poll();
-                if (consumer != null){
+			} else {
 
-                    consumer.setState(StatesConsumer.TAKE);
-                    countEnterStoreGood++;
+				stopWork(teller, standingLineConsumers, waitingStoreConsumers);
+				return;
+			}
 
-                }
+		}
 
-            }
+	}
 
-            countcon -= countEnterStoreGood;
+	public static void stopWork(Teller teller,
+			ConsumersQueueTeller standingLineConsumers,
+			ArrayBlockingQueue<Consumer> waitingStoreConsumers) {
 
+		if (teller != null) {
+			teller.setState(StatesTeller.RESTING);
+		}
 
+		while (!standingLineConsumers.isEmpty()) {
 
-        }
+			Consumer consumer = standingLineConsumers.poll();
+			if (consumer == null)
+				continue;
 
+			consumer.setState(StatesConsumer.GO_HOME);
 
+		}
 
-        while (true){
+		while (!waitingStoreConsumers.isEmpty()) {
 
-            Date currentDate = new Date();
-            long elapsedTime = currentDate.getTime() - beginWork.getTime();
-            if (elapsedTime < (long) timeWorkStore){
+			Consumer consumer = waitingStoreConsumers.poll();
+			if (consumer == null)
+				continue;
 
-                try {
-                    Thread.sleep(1000);
-                    continue;
-                }catch (InterruptedException e){
-                    stopWork(teller, standingLineConsumers, waitingStoreConsumers);
-                    return;
-                }
+			consumer.setState(StatesConsumer.GO_HOME);
 
-            }else {
+		}
 
-                stopWork(teller, standingLineConsumers, waitingStoreConsumers);
-                return;
-            }
+	}
 
-        }
+	public static String getModeDataStorage() {
 
-    }
+		return modeDataStorage;
 
+	}
 
-    public static void stopWork(Teller teller, ConsumersQueueTeller standingLineConsumers,
-                                            ArrayBlockingQueue<Consumer> waitingStoreConsumers){
+	private static void setModeDataStorage(String mode) {
 
-        if (teller != null){
-            teller.setState(StatesTeller.RESTING);
-        }
+		modeDataStorage = mode;
 
-        while (!standingLineConsumers.isEmpty()){
-
-            Consumer consumer = standingLineConsumers.poll();
-            if (consumer == null)
-                continue;
-
-            consumer.setState(StatesConsumer.GO_HOME);
-
-        }
-
-        while (!waitingStoreConsumers.isEmpty()){
-
-            Consumer consumer = waitingStoreConsumers.poll();
-            if (consumer == null)
-                continue;
-
-            consumer.setState(StatesConsumer.GO_HOME);
-
-        }
-
-    }
+	}
 
 }
--- Aschur_Store/src/management/SettingsLoader.java
+++ Aschur_Store/src/management/SettingsLoader.java
@@ -1,50 +1,172 @@
 package management;
 
 import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Aschur on 15.01.2017
  */
+
+
+
+/**
+ * @author Yulay
+ *
+ */
 public class SettingsLoader {
 
-    public static String[] getSettings() {
 
-        String[] settings = null;
-        InputStream inputStream = null;
-        StringBuilder sb = new StringBuilder();
+	/**
+	 * Settings - Map<String, String>:
+	 * modeDataStorage - "fileApp", "dataBase"
+	 * usageMode - "handMode", "automaticMode"
+	*/
+	public static Map<String, String> getPrimarySettings(){
 
-        try {
+		Map<String, String> settings = new HashMap<String, String>();
+		settings.put("modeDataStorage", "");
+		settings.put("usageMode", "");
 
-            ClassLoader myCL = SettingsLoader.class.getClassLoader();
-            inputStream = myCL.getResourceAsStream("Settings.txt");
+		String modeDataStorage = "";
+		String usageMode = "";
+		
+		InputStream inputStream = null;
+		StringBuilder sb = new StringBuilder();
 
-            try {
+		try {
 
-                int c;
-                while((c = inputStream.read()) != -1){
+			ClassLoader myCL = SettingsLoader.class.getClassLoader();
+			inputStream = myCL.getResourceAsStream("PrimarySettings.txt");
 
-                    sb.append(Character.toString((char) c));
+			try {
 
-                }
+				int c;
+				while ((c = inputStream.read()) != -1) {
 
-            }finally {
+					sb.append(Character.toString((char) c));
 
+				}
 
-                inputStream.close();}
+			} finally {
 
+				inputStream.close();
+			}
 
-        }
-        catch (Exception x)
-        {
-            x.printStackTrace();
-        }
+		} catch (Exception x) {
+			
+			x.printStackTrace();
+			
+			return settings;
+			
+		}
 
-        String allString = sb.toString().trim();
+		String allString = sb.toString().trim();
 
-        settings = allString.split("\n");
+		String[] primarySettings = allString.split("\r\n");
 
-        return settings;
+	
+		
+		if (primarySettings.length == 2) {
+			
+			modeDataStorage = primarySettings[0];
+			settings.put("modeDataStorage", modeDataStorage);
+			
+			usageMode = primarySettings[1];
+			settings.put("usageMode", usageMode);
+			
+		}
+		
+	
+		return settings;
 
-    }
+	}
 
-}
+	/**
+	 * Settings - Map<String, String>:
+	 * fileNameProcurementPlan
+	 * fileNameProducts
+	 * countConsumers
+	 * maxCountEnterStore
+	 * timeWorkStore
+	*/
+	public static Map<String, String> getSettings() {
+
+		Map<String, String> settings = new HashMap<String, String>();
+		settings.put("fileNameProcurementPlan", "");
+		settings.put("fileNameProducts", "");
+		settings.put("countConsumers", "");
+		settings.put("maxCountEnterStore", "");
+		settings.put("timeWorkStore", "");
+		
+		String modeDataStorage = Manager.getModeDataStorage();
+		if (modeDataStorage.equals("fileApp")) {
+
+			settings = getFileAppSettings(settings);
+			
+		} else {
+
+			settings = getDataBaseSettings(settings);
+			
+		}
+		
+	
+		return settings;
+
+	}
+
+	private static Map<String, String> getFileAppSettings(Map<String, String> settings) {
+
+		InputStream inputStream = null;
+		StringBuilder sb = new StringBuilder();
+
+		try {
+
+			ClassLoader myCL = SettingsLoader.class.getClassLoader();
+			inputStream = myCL.getResourceAsStream("FileAppSettings.txt");
+
+			try {
+
+				int c;
+				while ((c = inputStream.read()) != -1) {
+
+					sb.append(Character.toString((char) c));
+
+				}
+
+			} finally {
+
+				inputStream.close();
+			}
+
+		} catch (Exception x) {
+			
+			x.printStackTrace();
+			
+			return settings;
+			
+		}
+
+		String allString = sb.toString().trim();
+
+		String[] fileAppSettings = allString.split("\n");
+
+		if (fileAppSettings.length == 5) {
+			settings.put("fileNameProcurementPlan", fileAppSettings[0]);
+			settings.put("fileNameProducts", fileAppSettings[1]);
+			settings.put("countConsumers", fileAppSettings[2]);
+			settings.put("maxCountEnterStore", fileAppSettings[3]);
+			settings.put("timeWorkStore", fileAppSettings[4]);
+		}
+		
+		return settings;
+
+	}
+
+	private static  Map<String, String> getDataBaseSettings(Map<String, String> settings) {
+		
+		return settings;
+		
+	}
+	
+}	
